366291b214d29e1fe793c23c4845da4bee9376f1
Danuys <danuys@nusaciptaalumindo.com>
2025-09-03 11:50:22 +0700

    update huffman coding to use priority queue and adding a small improvement remove helo_test.go because its functionality is moved to huffman_test.go update to test files to split encoding and decoding test

--- huffman.go
+++ huffman.go
@@ -1,25 +1,115 @@
 /* Package gohuffman
 *  Implement huffman code in go
-*  Only contain 2 function "Compress" and "Decompress"
+*  Contain 4 function :
+*   - "Compress" and "Decompress" for Data Manipulation
+*   - "GetContent" and "GetHuffmanTable" for Data Reading
  */
 package main
 
 import (
-	"fmt"
-	"strings"
-	"unicode/utf8"
+	"container/heap"
 )
 
+type HuffmanTable struct {
+	encoding [256][]byte
+	decoding map[string]byte
+}
+
 type Data struct {
-	character  string
+	character  byte
 	recurring  uint8
 	childLeft  *Data
 	childRight *Data
 }
 
-var (
-	huffmanMap = make(map[string]string)
-	queue      []Data
+func (data *Data) isLeafNode() bool {
+	return data.childLeft == nil && data.childRight == nil
+}
+
+type Frequency struct {
+	buffer [256]*Symbol
+	symbols []*Symbol
+}
+
+type Symbol struct {
+ 	ascii byte
+	count uint8
+}
+
+func (f *Frequency) PopulateFrequencyTable(data []byte) {
+	for _, b := range data {
+		if f.buffer[b] == nil {
+			symbol := &Symbol{ascii: b, count: 0}
+			f.symbols = append(f.symbols, symbol)
+			f.buffer[b] = symbol
+		}
+		f.buffer[b].count++
+	}
+} 
+
+func nodeFromSymbol(symbol *Symbol) *Data {
+	return &Data{
+		character: symbol.ascii,
+		recurring: symbol.count,
+		childLeft: nil,
+		childRight: nil,
+	}
+}
+
+func joinNodes(a, b *Data) *Data {
+	return &Data {
+		character: 0,
+		recurring: a.recurring + b.recurring,
+		childLeft: a,
+		childRight: b,
+	}
+}
+
+// PriorityQueue implements heap.Interface and holds Items.
+type PriorityQueue []*Data
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	return pq[i].recurring < pq[j].recurring
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+}
+
+func (pq *PriorityQueue) Push(x any) {
+	item := x.(*Data)
+	*pq = append(*pq, item)
+}
+
+func (pq *PriorityQueue) Pop() any {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil // avoid memory leak
+	*pq = old[0 : n-1]
+	return item
+}
+
+func newFrequencyTable() *Frequency {
+	return &Frequency {
+		buffer: [256]*Symbol{},
+		symbols: make([]*Symbol, 0),
+	}
+}
+
+func newHuffmanTable() *HuffmanTable {
+	return &HuffmanTable {
+		encoding: [256][]byte{},
+		decoding: make(map[string]byte),
+	}
+}
+
+const (
+	HUFFMAN_ENCODE_LEN = 4
+	MAX_ENCODING_LEN = 8
+	CONTENT_LEN_IDX = 0
 )
 
 /*
@@ -32,101 +122,126 @@ var (
 5. Add the new node to the queue.
 6. The remaining node is the root node and the tree is complete.
 */
-func Compress(data string) string {
-	dataLength := utf8.RuneCountInString(data)
-	queue = make([]Data, 0, dataLength)
-	for _, c := range data {
-		found := false
-		for i, queueData := range queue {
-			if queueData.character == string(c) {
-				queueData.recurring++
-				queue[i] = queueData
-				found = !found
-				break
-			}
-		}
-		if found {
-			continue
-		}
-		queue = append(queue, Data{character: string(c), recurring: 1})
+func Compress(data []byte) ([]byte) {
+	frequencyTable := newFrequencyTable()
+	frequencyTable.PopulateFrequencyTable(data)
+	
+	queue := make(PriorityQueue, len(frequencyTable.symbols))
+
+	for i, symbol := range frequencyTable.symbols {
+		queue[i] = nodeFromSymbol(symbol)
 	}
 
-	for {
-		limit := len(queue)
-		if limit == 1 {
-			break
-		}
-		for i := 1; i < limit; i++ {
-			num1 := queue[i]
-			for j := i - 1; j >= 0; j-- {
-				if num1.recurring > queue[j].recurring {
-					queue[j+1] = queue[j]
-					queue[j] = num1
-				}
-			}
-		}
+	heap.Init(&queue)
 
-		childLeft := queue[limit-1]
-		childRight := queue[limit-2]
-		lowestTwo := Data{character: queue[limit-1].character + queue[limit-2].character, recurring: queue[limit-1].recurring + queue[limit-2].recurring, childLeft: &childLeft, childRight: &childRight}
-		queue[limit-2] = lowestTwo
-		queue = queue[:limit-1]
-		fmt.Printf(" data is %s and lchild is %s , rchild is %s -> ", lowestTwo.character, lowestTwo.childLeft.character, lowestTwo.childRight.character)
+	for len(queue) > 1  {
+		nodeL := heap.Pop(&queue).(*Data)
+		nodeR := heap.Pop(&queue).(*Data)
 
-		for _, queueData := range queue {
-			fmt.Printf("%s %d ", queueData.character, queueData.recurring)
-		}
-		fmt.Printf("\n")
+		heap.Push(&queue, joinNodes(nodeL, nodeR))
 	}
 
-	fmt.Printf(" dataLength is %d\n", len(queue))
-	traverseTree(queue[0], make([]rune, 0, len(queue[0].character)))
+	root := heap.Pop(&queue).(*Data)
+	treeLen := len(frequencyTable.symbols) * 2 - 1
 
-	var compressed strings.Builder
-	for _, character := range data {
-		compressed.WriteString(huffmanMap[string(character)])
-	}
+	// Array Structure is 4 byte for each node
+	// [ value, count, left, right ]
+	index := 0
+	huffmanTree := make([]byte, treeLen * HUFFMAN_ENCODE_LEN)
+	makeHuffmanTree(root, huffmanTree, &index)
 
-	return compressed.String()
+	table := newHuffmanTable()
+
+	populateHuffmanTable(huffmanTree, table)
+
+	result, length := encodeData(data, *table)
+
+	join := make([]byte, 1, index + length + 1) 
+	join[CONTENT_LEN_IDX] = byte(length + 1) 
+	join = append(join, result...)
+	join = append(join, huffmanTree...)
+
+	return join
 }
 
-func traverseTree(data Data, path []rune) {
-	var paths strings.Builder
-	for _, i := range path {
-		paths.WriteString(string(i))
-	}
-
-	fmt.Printf("%s -> %s:%d \n", paths.String(), data.character, data.recurring)
-	huffmanMap[data.character] = paths.String()
-	if data.childLeft != nil {
-		lpath := append(path, '0')
-		traverseTree(*data.childLeft, lpath)
-	}
-	if data.childRight != nil {
-		rpath := append(path, '1')
-		traverseTree(*data.childRight, rpath)
-	}
+func GetContent(data []byte) []byte {
+	length := data[CONTENT_LEN_IDX]
+	return data[1:length]
 }
 
-func translateTree(data Data, path rune) (*Data, *string) {
-	if path == '0' {
-		if data.childLeft != nil {
-			return data.childLeft, nil
-		}
+func GetHuffmanTree(data []byte) []byte {
+	start := data[CONTENT_LEN_IDX]
+	return data[start:]
+}
+
+func encodeData(data []byte, table HuffmanTable) ([]byte, int) {
+	encodedData := make([]byte, 0, len(data)*MAX_ENCODING_LEN)
+	for _, b := range data {
+		encodedData = append(encodedData, table.encoding[b]...)
+	}
+	return encodedData, len(encodedData)
+}
+
+func populateHuffmanTable(huffmanTree []byte, table *HuffmanTable) {
+	if huffmanTree == nil {
+		return
+	}
+	readHuffmanTree(huffmanTree, []byte{}, 0, table)
+}
+
+func readHuffmanTree(tree []byte, value []byte, index int, table *HuffmanTable) {
+	if tree[index] == 0 {
+		readHuffmanTree(tree, append(value, '0'), int(tree[index+2]), table)
+		readHuffmanTree(tree, append(value, '1'), int(tree[index+3]), table)
 	} else {
-		if data.childRight != nil {
-			return data.childRight, nil
-		}
+		leaf := make([]byte, len(value))
+		copy(leaf, value)
+
+		table.encoding[tree[index]] = leaf
+		table.decoding[string(leaf)] = tree[index]
 	}
-	return nil, &data.character
 }
 
-func Decompress(data string) string {
-	var decompressed string
-	copy := queue[0]
-	for _, character := range data {
-		translateTree(copy, character)
+func makeHuffmanTree(data *Data, tree []byte, index *int) {
+	if data == nil {
+		return  
 	}
 
+	base := *index
+
+	tree[base] = data.character
+	tree[base+1] = data.recurring
+
+	*index += HUFFMAN_ENCODE_LEN
+
+	if data.isLeafNode() {
+		tree[base+2] = 0 
+		tree[base+3] = 0 
+		return
+	}
+
+	tree[base+2] = byte(*index)
+	makeHuffmanTree(data.childLeft, tree, index)
+
+	tree[base+3] = byte(*index)
+	makeHuffmanTree(data.childRight, tree, index)
+}
+
+func Decompress(data []byte) []byte {
+	table := newHuffmanTable()
+	populateHuffmanTable(GetHuffmanTree(data), table)
+
+	content := GetContent(data)
+
+	decompressed := make([]byte, 0, data[0]) 
+	helper := make([]byte, 0, MAX_ENCODING_LEN)
+	for _, b := range content {
+		helper = append(helper, b)
+		value, found := table.decoding[string(helper)]
+		if found  {
+			decompressed = append(decompressed, value)
+			helper = helper[:0]
+		}
+	}
 	return decompressed
 }
--- huffman_test.go
+++ huffman_test.go
@@ -1,16 +1,56 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
-	"net/http"
-	"time"
+	"testing"
 )
 
-func greet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World! %s", time.Now())
+func TestHuffmanEncoding(t *testing.T) {
+	sample := []byte("A_DEAD_DAD_CEDED_A_BAD_BABE_A_BEADED_ABACA_BED")
+	expected := []byte("1000011101001000110010011101100111001001000111110010011111011111100010001111110100111001001011111011101000111111001")
+	data := Compress(sample)
+
+	result := GetContent(data)
+	tree := GetHuffmanTree(data)
+
+	xor := make([]byte, len(expected))
+	for i := range len(xor) {
+		xor[i] = expected[i]^result[i]
+		if xor[i] > 0 {
+			fmt.Printf("%d -> xor %b (%s) with %b (%s) be %b \n", i, expected[i], string(expected[i]),result[i], string(result[i]), xor[i])
+		}
+	}
+
+    fmt.Printf("expected (%d)\t '%s'\n result (%d)\t '%s'\n with xor\t '%s'\n using (%d)\t %v\n", len(expected), string(expected), len(result), string(result), string(xor), len(tree), tree)
+
+	if string(result) != string(expected) {
+		t.Errorf("should be \n '%s' but got \n '%s'\n", expected, result)
+	}
 }
 
-func main() {
-	http.HandleFunc("/", greet)
-	http.ListenAndServe(":8080", nil)
+func TestHuffmanDecoding(t *testing.T) {
+	sample := bytes.Join([][]byte{
+		{116},
+		[]byte("1000011101001000110010011101100111001001000111110010011111011111100010001111110100111001001011111011101000111111001"),
+		{0,46,4,16,0,20,8,12,95,10,0,0,68,10,0,0,0,26,20,24,65,11,0,0,0,15,28,32,69,7,0,0,0,8,36,40,67,2,0,0,66,6,0,0},
+	},[]byte{})
+	expected := []byte("A_DEAD_DAD_CEDED_A_BAD_BABE_A_BEADED_ABACA_BED")
+
+	result := Decompress(sample)
+	tree := GetHuffmanTree(sample)
+
+	xor := make([]byte, len(expected))
+	for i := range len(xor) {
+		xor[i] = expected[i]^result[i]
+		if xor[i] > 0 {
+			fmt.Printf("%d -> xor %b (%s) with %b (%s) be %b \n", i, expected[i], string(expected[i]),result[i], string(result[i]), xor[i])
+		}
+	}
+
+    fmt.Printf("expected (%d)\t '%s'\n result (%d)\t '%s'\n with xor\t '%s'\n using (%d)\t %v\n", len(expected), string(expected), len(result), string(result), string(xor), len(tree), tree)
+	
+	if string(result) != string(expected) {
+		t.Errorf("should be \n '%s' but got \n '%s'\n", expected, result)
+	}
 }
